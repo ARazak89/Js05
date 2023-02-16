3275aa638b981ddc77a8248f8c6d6c2ceeaa579a
ARazak89 <124513107+ARazak89@users.noreply.github.com>
2023-02-16 08:44:49 +0100

    Corrections
    Affichage à l'intérieur de la fonction
    déclaration de la chaine de caractères à l'intérieur de la fonction

--- countAcaracter.js
+++ countAcaracter.js
@@ -1,4 +1,4 @@
-function countAcaracter(sentence){
+function countAcaracter(){
     let sentence = "Bonjour, chers candidat comment vous allez aujourd'hui";
     let count = 0;
 for(let i = 0; i < sentence.length; i++)
